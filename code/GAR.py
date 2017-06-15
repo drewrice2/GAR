7f0105d1b6e0e0e2da509d5fe015dcdbb8c223f1
Drew Rice <drice@capson.com>
2017-06-15 09:55:28 -0500

    formatting, function descriptions

--- code/GAR.py
+++ code/GAR.py
@@ -21,8 +21,18 @@ class Genome:
 
     def __init__(self, net_must_start_with, net_must_end_with, min_depth=4,
             max_depth=7, dimensionality=2):
-        '''
-        # Accepts relevant variables, type checks.
+        '''Accepts relevant variables, type checks.
+
+        # Arguments
+            net_must_start_with: list of dictionaries with shape: [{'layer_name':{'param_name':parameter}}]
+            net_must_end_with: list of dictionaries. [{'layer_name':{'param_name':parameter}}]
+            min_depth: minimum number of *core* layers for the net
+            max_depth: maximum number of *core* layers for the net
+            dimensionality: true input data dimensionality
+
+        # Raises
+            ValueError: if `dimensionality` != 2, or if `max_depth` < `min_depth`
+            TypeError: if input vars are of incorrect type.
         '''
         self.net_must_start_with = net_must_start_with
         self.net_must_end_with = net_must_end_with
@@ -32,7 +42,7 @@ class Genome:
         self.net_depth = random.randint(min_depth, max_depth)
         self.model = Sequential() # only supporting Sequential models initially
         self.architecture = []
-        # possibile criteria for randomization
+        # randomization universe
         self.node_range = [16,32,64,128,256]
         self.conv_filter_range = [16,32,64,128,256]
         self.dropout_range = [0.1,0.25,0.5]
@@ -71,8 +81,11 @@ class Genome:
         typecheck_and_error_handle()
 
     def build(self):
-        '''
-        # Runs all network randomization and building.
+        '''Runs all network randomization and building.
+
+        # Returns
+            model: an uncompiled Keras model object with GAR randomized layers
+            architecture: list of dictionaries for calling GAR on, or for logging purposes
         '''
         self.add_from_list(self.net_must_start_with)
         self.randomize_layers()
@@ -80,8 +93,9 @@ class Genome:
         return self.model, self.architecture
 
     def randomize_layers(self):
-        '''
-        # Randomize layers until `self.max_depth` is reached.
+        '''Randomize layers until `self.net_depth` is reached. Only working for 2D convolutional nets
+            at the moment. Current workflow: randomized `num_conv_layers`, with randomized pooling, and
+            randomized `num_fully_connected`, with randomized dropout.
         '''
         # only supporting 2D nets at the moment, but ¯\_(ツ)_/¯
         if self.dimensionality == 2:
@@ -113,12 +127,13 @@ class Genome:
                     self.add_layer_dict_to_model(dropout_layer)
 
     def add_from_list(self, list_of_layers):
-        '''
-        # Input: list_of_layers to run through `self.interpret_layer_dict`.
-            Called on `self.net_must_start_with`, `self.net_must_end_with`.
+        '''Adds a given list of layers to the model.
 
-        # Raises: TypeError
-                    if layer parameters is not a dictionary.
+        # Arguments
+            list_of_layers: list, passing one at a time to `self.interpret_layer_dict`.
+
+        # Raises
+            TypeError: if layer parameters is not a dictionary.
         '''
         for input_layer in list_of_layers:
             if type(input_layer) != dict:
@@ -129,14 +144,14 @@ class Genome:
                 self.add_layer_dict_to_model(layer)
 
     def add_layer_dict_to_model(self, layer_dictionary):
-        '''
-        # Receives a one-layer dictionary. Interprets and adds to `self.model`.
+        '''Receives a one-layer dictionary. Interprets and adds to `self.model`.
 
-        # Input: `layer_dictionary` is a one-layer dictionary {`layer_name`:`params`}.
-            Appends `layer_dictionary` to `self.architecture` list.
+        # Arguments
+            layer_dictionary: one-layer dictionary {`layer_name`:{`param_name`:`parameter`}}. Appends
+                `layer_dictionary` to `self.architecture` list.
 
-        # Raises: ValueError
-                    if layer is not supported.
+        # Raises
+            ValueError: if layer is not supported.
         '''
         layer = ''
         parameters = {}
@@ -165,11 +180,12 @@ class Genome:
         self.architecture.append(layer_dictionary)
 
     def interpret_layer_dict(self, layer_dictionary):
-        '''
-        # Interprets a single-layer dictionary.
+        '''Interprets a single-layer dictionary. If correct parameters exist, pass them to
+            `keras_layer_parameters`, else randomly generate from randomization universe.
 
-        # Returns: dictionary of `layer_name`:`parameters`
-            where the `parameters` dict is fed directly into a Keras layer object
+        # Returns
+            layer_to_keras: dictionary of shape {`layer_name`:{`param_name`: `parameter`}}
+                where the `parameters` dict is fed directly into a Keras layer object
 
         # TODO: support Conv1D, LocallyConnected1D, LocallyConnected2D
         '''
@@ -232,140 +248,10 @@ class Genome:
             msg = 'Could not find `%s` in supported layers.' % (layer_name)
             raise ValueError(msg)
 
-        return {layer_name:keras_layer_parameters}
+        layer_to_keras = {layer_name: keras_layer_parameters}
+        return layer_to_keras
 
     def clear_memory():
-        '''
-        # Delete any large variables from memory. TBD on what they are.
+        '''Delete any large variables from memory. TBD on what they are yet.
         '''
         pass
-
-if __name__ == '__main__':
-    x = Genome('b','b',dimensionality=2, min_depth=4,max_depth=4)
-
-# def layer_add(layer_name,
-#         node_range=[16,32,64,128,256], \
-#         dropout_range=[0.1,0.25,0.5], \
-#         pool_or_kernel_range_2D=[(2,2),(3,3),(4,4)], \
-#         pool_or_kernel_range_1D=[2,3,4,6], \
-#         activation_funcs=['relu']):
-#     '''
-#     # Generate a layer from a given layer name.
-#
-#     # Supported layers:
-#         Dense, Dropout, Flatten, Conv2D, MaxPooling2D, LocallyConnected2D
-#
-#         Future layers:
-#             MaxPooling1D, Conv1D, LocallyConnected1D, BatchNormalization
-#
-#     # Raises:
-#         ValueError, if 'layer_name' is not recognized
-#
-#     # Returns: layer object.
-#
-#     # TODO! accept 'parameters' dictionary with 'layer_name','num_nodes','activation_func'
-#     '''
-#     # random selections done upfront
-#     layer_name = layer_name.lower()
-#     node_size = random.choice(node_range)
-#     activation = random.choice(activation_funcs)
-#     kernel_or_pool_size_2d = random.choice(pool_or_kernel_range_2D)
-#     kernel_or_pool_size_1d = random.choice(pool_or_kernel_range_1D)
-#
-#     if layer_name == 'dense':
-#         layer = Dense(units=node_size, activation=activation)
-#     elif layer_name == 'dropout':
-#         layer = Dropout(random.choice(dropout_range))
-#     elif layer_name == 'flatten':
-#         layer = Flatten()
-#     elif layer_name == 'conv1d':
-#         model.add(Conv1D(filters=node_size, kernel_size=kernel_or_pool_size_1d, activation=activation))
-#     elif layer_name == 'conv2d':
-#         layer = Conv2D(filters=node_size, kernel_size=kernel_or_pool_size_2d, activation=activation)
-#     elif layer_name == 'maxpooling1d':
-#         layer = MaxPooling1D(pool_size=pool_or_kernel_range_1D)
-#     elif layer_name == 'maxpooling2d':
-#         layer = MaxPooling2D(pool_size=pool_or_kernel_range_2D)
-#     elif layer_name == 'locallyconnected1d':
-#         layer = LocallyConnected1D(filters=node_size, kernel_size=pool_or_kernel_range_1D, activation=activation)
-#     elif layer_name == 'locallyconnected2d':
-#         layer = LocallyConnected2D(filters=node_size, kernel_size=pool_or_kernel_range_2D, activation=activation)
-#     else: # layer unrecognized and not added
-#         msg = 'Could not find "%s" in supported layers. \n\tError occurred at: %s' % \
-#             (layer_name, datetime.now().strftime('%Y-%m-%d %H:%M:%S UTC'))
-#         raise ValueError(msg)
-#     return layer
-#
-# def add_from_list(model, layer_list, model_architecture_list):
-#     '''
-#     # Recevies a list and adds the given layers to the model.
-#
-#     # Returns: model architecture list.
-#     '''
-#     for layer_name in layer_list:
-#         try:
-#             layer_to_add = layer_add(layer_name)
-#             model_architecture_list.append(layer_name)
-#             model.add(layer_to_add)
-#         except ValueError as e:
-#             pass
-#     return model_architecture_list
-#
-# def generate_genome(model, dimensionality, min_depth=2, max_depth=7, net_must_start_with=[], net_must_end_with=[]):
-#     '''
-#     # Generate basic genome from given dimension, parameters.
-#
-#     # TODO:
-#         add ability to specify layer-specific parameters on opening and closing, i.e. node_size
-#
-#     # Raises:
-#         ValueError, if 'min_depth' and 'max_depth' are incorrectly sized
-#         TypeError, if 'net_must_start_with' & 'net_must_end_with' are not lists
-#     '''
-#     # ERROR HANDLING !
-#     # check depth args
-#     if min_depth >= max_depth:
-#         msg = 'Minimum depth variable "%i" needs to be bigger than max_depth variable "%i".\n\tError occurred at: %s' % \
-#             (min_depth, max_depth, datetime.now().strftime('%Y-%m-%d %H:%M:%S UTC'))
-#         raise ValueError(msg)
-#     # check net_must_start_with & net_must_end_with data types
-#     if type(net_must_start_with) != list:
-#         msg = 'Argument "net_must_start_with" must be a list.\n\tError occurred at: %s' % \
-#             (datetime.now().strftime('%Y-%m-%d %H:%M:%S UTC'))
-#         raise TypeError(msg)
-#     if type(net_must_end_with) != list:
-#         msg = 'Argument "net_must_end_with" must be a list.\n\tError occurred at: %s' % \
-#             (datetime.now().strftime('%Y-%m-%d %H:%M:%S UTC'))
-#         raise TypeError(msg)
-#
-#     # check dimensionality, define universe of available functions
-#     # TODO: infer from input dimensions
-#     if dimensionality == 2:
-#         available_funcs = ['conv2d','dense','dropout','maxpooling2d'] # ,'locallyconnected2d']
-#     elif dimensionality == 1:
-#         # available_funcs = ['dense','dropout','conv1d','maxpooling1d','locallyconnected1d']
-#         # only supporting 2 dimensional data at this point
-#         msg = 'Not supporting 1D...Only supporting 2 dimensional data at this point.'
-#         raise ValueError(msg)
-#     else:
-#         msg = 'Dimensionality must be "1" or "2".\n\tError occurred at: %s' % \
-#             (datetime.now().strftime('%Y-%m-%d %H:%M:%S UTC'))
-#         raise ValueError(msg)
-#     # generate architecture
-#     model_architecture = []
-#     net_size = random.randint(min_depth, max_depth)
-#     # add must_start_with
-#     model_architecture = add_from_list(model, net_must_start_with, model_architecture)
-#     for _ in range(net_size): # being done one layer at a time to only generate working nets
-#         while True:
-#             layer = np.random.choice(available_funcs, 1, p=[0.35,0.35,0.1,0.2])[0]
-#             try:
-#                 layer_to_add = layer_add(layer)
-#                 model_architecture.append(layer)
-#                 model.add(layer_to_add)
-#                 break
-#             except ValueError as e:
-#                 pass
-#     # add must_end_with
-#     model_architecture = add_from_list(model, net_must_end_with, model_architecture)
-#     return model_architecture
